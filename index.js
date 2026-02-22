9e29ff72120b40fea7fa1a98068c3e51277d535d
krossauchik0-blip <krossauchik0@gmail.com>
2026-02-22 16:38:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-импортировать TelegramBot из 'node-telegram-bot-api';
-импортирование аксиос из 'аксиос';
+import TelegramBot from 'node-telegram-bot-api';
+import axios from 'axios';
 
-const bot = new TelegramBot(process.env.BOT_TOKEN, { пример: true });
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
-bot.on('сообщение', асинхронный (сообщение) => {
- попробуй {
- константый ответ = ожидание axios.post(
- "https://cloud.flowiseai.com/api/v1/prediction/5ecfc67f-e2dd-4166-9a33-c00456cb64cd",
- { ввод: msg.text }
- );
+bot.on('message', async (msg) => {
+  try {
+    const response = await axios.post(
+      "https://cloud.flowiseai.com/api/v1/prediction/5ecfc67f-e2dd-4166-9a33-c00456cb64cd",
+      { input: msg.text }
+    );
 
- bot.sendMessage(msg.chat.id, response.data.text);
- } поймать (ошибка) {
- bot.sendMessage(msg.chat.id, "Ошибка ИИ");
+    bot.sendMessage(msg.chat.id, response.data.text);
+  } catch (err) {
+    bot.sendMessage(msg.chat.id, "Ошибка AI");
   }
 });
