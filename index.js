3aedd8f141724367f58a8839fa39b796306b2ed9
krossauchik0-blip <krossauchik0@gmail.com>
2026-02-22 17:57:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-import TelegramBot from 'node-telegram-bot-api';
-import axios from 'axios';
+импорт TelegramBot от 'node-telegram-bot-api';
+импорт аксиос от 'аксиос';
 
-const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+константа бот = новый TelegramBot(процесс.энв.БОТ_ТОКЕН, { опрос: истинный });
 
-bot.on('message', async (msg) => {
-  try {
-    const response = await axios.post(
+бот.на('сообщение', асинхронный (сообщение) => {
+  пытаться {
+    константа ответ = ждать аксиос.почта(
       "https://cloud.flowiseai.com/api/v1/prediction/5ecfc67f-e2dd-4166-9a33-c00456cb64cd",
       {
-        input: msg.text,
-        overrideConfig: {
-          sessionId: msg.chat.id.toString()
+        вход: сообщение.текст,
+        переопределитьКонфигурацию: {
+          sessionId: сообщение.чат.идентификатор.toString()
         }
       }
     );
 
-    const reply = response.data.text || response.data.answer || "Нет ответа";
+    константа отвечать = ответ.данные.текст || ответ.данные.отвечать || "Нет ответа";
 
-    bot.sendMessage(msg.chat.id, reply);
+    бот.отправитьСообщение(сообщение.чат.идентификатор, отвечать);
 
-  } catch (err) {
-    console.error(err.response?.data || err.message);
-    bot.sendMessage(msg.chat.id, "Ошибка AI");
+  } ловить (ошибаться) {
+    консоль.ошибка(ошибаться.ответ?.данные || ошибаться.сообщение);
+    бот.отправитьСообщение(сообщение.чат.идентификатор, "Ошибка ИИ");
   }
 });
